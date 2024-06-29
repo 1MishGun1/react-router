6a9cd1f8d32720b8f09bff944b2a5595e1b47b46
1MishGun1 <mikhail.kudrov@gmail.com>
2024-06-29 20:35:21 +0300

    add .item_link

--- src/pages/DetailsPage.jsx
+++ src/pages/DetailsPage.jsx
@@ -1,5 +1,5 @@
-import { useParams, Link } from 'react-router-dom';
-import { data } from '../data/data';
+import { useParams, Link } from "react-router-dom";
+import { data } from "../data/data";
 
 export const DetailsPage = () => {
   const { id } = useParams();
@@ -14,8 +14,15 @@ export const DetailsPage = () => {
       <h2>{item.title}</h2>
       <h3>Developer: {item.developer}</h3>
       <p>Description: {item.description}</p>
-      <iframe src={item.link} width={1000} height={700}></iframe>
-      <Link to={item.link}>Play</Link>
+      <iframe
+        src={item.link}
+        width={1000}
+        height={700}
+        title={item.title}
+      ></iframe>
+      <Link to={item.link} className="item_link">
+        Play
+      </Link>
     </div>
   );
 };
